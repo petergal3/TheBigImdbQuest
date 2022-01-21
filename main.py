9a3107822a5707fccb4de81c2c1ddd83a69b547f
petergal3 <peterga3@gmail.com>
2022-01-21 13:14:56 +0100

    film scraper ready

--- main.py
+++ main.py
@@ -1,27 +1,24 @@
 import unittest
 from selenium import webdriver
+from page import *
 
 class Webscraper(unittest.TestCase):
 
     def setUp(self):
         print("Setup")
         self.driver = webdriver.Chrome("chromedriver.exe")
-        self.driver.get("https://www.imdb.com/")
+        self.driver.get("https://www.imdb.com/chart/top")
         print("Setup finished")
 
     def test_search(self):
+        filmhandler = FilmHandler(self.driver)
+        filmhandler.is_title_matches()
+        filmhandler.Scraper()
         print("test")
 
     def tearDown(self):
-            print("End")
+        print("End")
 
 
 if __name__ == '__main__':
    unittest.main(exit=False)
-
-
-
-
-
-
-
